50039e6c2cf696b31b53b71c7886baccebb815ad
Berkay Ülger <berkayulger45920@gmail.com>
2023-12-09 10:21:13 +0300

    a

--- appMy/models.py
+++ appMy/models.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import User
 
 
 class Blog(models.Model):
-   user = models.ForeignKey(User, verbose_name=("Kullanıcı"), on_delete=models.CASCADE)
+   user = models.ForeignKey(User,related_name="user1" ,verbose_name=("Kullanıcı"), on_delete=models.CASCADE)
    title = models.CharField(("Başlık"), max_length=50)
    text = models.TextField(("Blog Yazısı"))
    image = models.ImageField(("Resim"), upload_to="blog")
    date_now = models.DateTimeField(("Tarih - Saat"), auto_now_add=False)
-   
+   likes = models.ManyToManyField(User,related_name="user2", verbose_name=("Beğenen Kullanıcılar"))
    
    def __str__(self) -> str: # admin panelinde obje ismi tanımlama
       return self.title
--- appMy/views.py
+++ appMy/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from appMy.models import *
+# from django.db.models import Count
+
 # Create your views here.
 
 def indexPage(request):
-
    blog_list = Blog.objects.all()
+   # blog_likes = Blog.objects.annotate(q_count=Count('likes')).order_by('-q_count')
    
+   
+   print(blog_likes)
    context = {
       "blog_list":blog_list,
    }
    return render(request, 'index.html',context)
+
+def detailPage(request, bid):
+   blog = Blog.objects.get(id=bid)
+   context = {
+      "blog":blog,
+   }
+   return render(request, "detail.html", context)
